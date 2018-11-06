93d54ba8a1f8297e80a9abb4547fbf88d88ac087
Marc Mills <mdmills1990@gmail.com>
2018-11-06 21:51:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,11 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
     @@items.each do |item|
       resp.write "#{item}\n"
+    else
+      resp.write "Path not found, bitch."
     end
     resp.finish
   end
